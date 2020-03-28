85a8abaeca09056a8ec629a76e5c97efd7572cee
zmader <47906967+zmader@users.noreply.github.com>
2020-03-28 11:01:22 -1000

    worked on localGame and gameState

--- app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueGameState.java
+++ app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueGameState.java
@@ -2,21 +2,49 @@ package edu.up.cs301.game.GameFramework.Clue;
 
 import android.widget.ProgressBar;
 
-public class ClueGameState
+import edu.up.cs301.game.GameFramework.infoMessage.GameState;
+
+public class ClueGameState extends GameState
 {
 
     //private gameStage;
     private int[] rollResult;
     private TileData[][] board;
     private Card[] suggestedCards;
+    private int moves;
+    //maybe have a variable for all cards dealt, and it is accessed using player ID int
+    //if any variables are added here, make sure to assign them in the two constuctors
 
-    public Player getWhoseTurn()
+
+    //some stuff for interface
+    private static final long serialVersionUID = 7737393762469851826L;
+
+    public ClueGameState()
     {
-
+        //empty constructor, this will assign the board and choose cards for players
     }
+
+    public ClueGameState(ClueGameState or) {
+        rollResult = or.rollResult;
+        suggestedCards = or.suggestedCards;
+        board = or.board;
+        //if other variables are added, they need to be added here too
+    }
+
+
+    //public Player getWhoseTurn()
+    //{
+
+    //}
 
     public int getMovesLeft()
     {
+        return moves;
+    }
 
+    public void setRollResult(int[] roll)
+    {
+        this.rollResult = roll;
+        moves = rollResult[0] + rollResult[1];
     }
 }
--- app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueLocalGame.java
+++ app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueLocalGame.java
@@ -3,55 +3,81 @@ package edu.up.cs301.game.GameFramework.Clue;
 import android.widget.ProgressBar;
 
 import edu.up.cs301.game.GameFramework.GamePlayer;
+import edu.up.cs301.game.GameFramework.LocalGame;
+import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
 
-public class ClueLocalGame
+public class ClueLocalGame extends LocalGame
 {
     //private Player whoseTurn;
     private int movesLeft;
     //private Card[] winningCards;
+    private ClueGameState gameState;
+
 
     public ClueLocalGame()
     {
-
+        this.gameState = new ClueGameState();
     }
 
-    private GamePlayer disproveTurn()
-    //{
+    @Override
+    protected String checkIfGameOver()
+    {
+        return null; //put actual content here
+    }
 
-    //}
+    @Override
+    protected void sendUpdatedStateTo(GamePlayer pl)
+    {
+        pl.sendInfo(new ClueGameState(gameState));
+    }
+
+    @Override
+    protected boolean canMove(int pl)
+    {
+        return false;
+    }
+
+    @Override
+    protected boolean makeMove(GameAction action)
+    {
+        //this method checks which state of the game we are in, and if a certain move can be made then
+        //updated the gameState if the move was valid, and returns false, else returns false
+        return false; //filler
+    }
+
+
+    private GamePlayer disproveTurn()
+    {
+        return null;
+    }
 
     public boolean canRoll(GamePlayer pl)
     {
-
-    }
-
-    public boolean canMove(GamePlayer pl)
-    {
-
+        return false;
     }
 
     public boolean canAccuse(GamePlayer pl)
     {
-
+        return false;
     }
 
     public boolean canSuggest(GamePlayer pl)
     {
-
+        return false;
     }
 
     public boolean canDisprove(GamePlayer pl)
     {
-
+        return false;
     }
 
     public boolean canPassageway(GamePlayer pl)
     {
-
+        return false;
     }
 
     public boolean checkAccusation(Card[])
     {
-
+        return false;
     }
 }
