9124796f2d7c1357041ee21e8939ab082caf9dcc
zmader <47906967+zmader@users.noreply.github.com>
2020-03-28 13:28:02 -1000

    main activity class completed (without ai added)

--- app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueMainActivity.java
+++ app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueMainActivity.java
@@ -1,25 +1,51 @@
 package edu.up.cs301.game.GameFramework.Clue;
 
+import java.util.ArrayList;
+
+import edu.up.cs301.game.GameFramework.GameMainActivity;
+import edu.up.cs301.game.GameFramework.GamePlayer;
 import edu.up.cs301.game.GameFramework.LocalGame;
 import edu.up.cs301.game.GameFramework.gameConfiguration.GameConfig;
+import edu.up.cs301.game.GameFramework.gameConfiguration.GamePlayerType;
 
-public class ClueMainActivity
+public class ClueMainActivity extends GameMainActivity
 {
     public LocalGame localGame;
     public GameConfig gameConfig;
+
+    private static final int PORT_NUMBER = 6598;
+
+    @Override
+    public GameConfig createDefaultConfig()
+    {
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+        //human player type
+        playerTypes.add(new GamePlayerType() {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new ClueHumanPlayer(name);
+            }
+        });
+
+        //create configuration
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 6, "Clue Game", PORT_NUMBER);
+
+        //add players to config
+        //update with different AI as game is developed
+        defaultConfig.addPlayer("Human", 0);
+
+        return defaultConfig;
+
+    }
 
     public ClueMainActivity()
     {
 
     }
 
-    public void createLocalGame()
+    public LocalGame createLocalGame()
     {
-
-    }
-
-    public void createDefaultConfig()
-    {
-
+        return new ClueLocalGame();
     }
 }
