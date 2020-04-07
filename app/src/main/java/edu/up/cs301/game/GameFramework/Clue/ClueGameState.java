1b1361f1fe21255dd9b6f07cf8b6516636b90d8c
zmader <47906967+zmader@users.noreply.github.com>
2020-04-06 14:18:15 -1000

    make cards and shuffles them

--- app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueGameState.java
+++ app/src/main/java/edu/up/cs301/game/GameFramework/Clue/ClueGameState.java
@@ -11,6 +11,7 @@ public class ClueGameState extends GameState
     private int[] rollResult;
     private TileData[][] board;
     private Card[] suggestedCards;
+    private Card[] winningCards = new Card[3];
     private int moves;
     private int whoseMove;
     //maybe have a variable for all cards dealt, and it is accessed using player ID int
@@ -23,6 +24,8 @@ public class ClueGameState extends GameState
     public ClueGameState()
     {
         //empty constructor, this will assign the board and choose cards for players
+
+
     }
 
     public ClueGameState(ClueGameState or) {
