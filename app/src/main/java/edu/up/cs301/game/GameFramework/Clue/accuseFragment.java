5181db2afd7341eb297abfe8b5fb381b479b2b5d
Kai Richardson <kairichardson@gmail.com>
2020-04-09 00:00:01 -0700

    makes buttons work on sugg/acc screens

--- app/src/main/java/edu/up/cs301/game/GameFramework/Clue/accuseFragment.java
+++ app/src/main/java/edu/up/cs301/game/GameFramework/Clue/accuseFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 import edu.up.cs301.game.R;
 
@@ -28,10 +29,60 @@ public class accuseFragment extends Fragment implements View.OnClickListener
 	{
 		View myView = inflater.inflate(R.layout.clue_accuse_layout, container, false);
 
-		//do whatever you want here - like adding a listview or anything
-
+		//Adds confirm/cancel buttons
 		Button cancelButtonAcc = myView.findViewById(R.id.cancelButtonAcc);
 		cancelButtonAcc.setOnClickListener(this);
+
+		Button confirmButtonA = myView.findViewById(R.id.confirmButtonA);
+		confirmButtonA.setOnClickListener(this);
+
+		//Character Side Buttons
+		ToggleButton scarletButton = myView.findViewById(R.id.scarletButton);
+		scarletButton.setOnClickListener(this);
+		ToggleButton peacockButton = myView.findViewById(R.id.peacockButton);
+		peacockButton.setOnClickListener(this);
+		ToggleButton greenButton = myView.findViewById(R.id.greenButton);
+		greenButton.setOnClickListener(this);
+		ToggleButton plumButton = myView.findViewById(R.id.plumButton);
+		plumButton.setOnClickListener(this);
+		ToggleButton whiteButton = myView.findViewById(R.id.whiteButton);
+		whiteButton.setOnClickListener(this);
+		ToggleButton mustardButton = myView.findViewById(R.id.mustardButton);
+		mustardButton.setOnClickListener(this);
+
+		//Weapon Side Buttons
+		ToggleButton knifeButton = myView.findViewById(R.id.knifeButton);
+		knifeButton.setOnClickListener(this);
+		ToggleButton revolverButton = myView.findViewById(R.id.revolverButton);
+		revolverButton.setOnClickListener(this);
+		ToggleButton candlestickButton = myView.findViewById(R.id.candlestickButton);
+		candlestickButton.setOnClickListener(this);
+		ToggleButton ropeButton = myView.findViewById(R.id.ropeButton);
+		ropeButton.setOnClickListener(this);
+		ToggleButton leadpipeButton = myView.findViewById(R.id.leadpipeButton);
+		leadpipeButton.setOnClickListener(this);
+		ToggleButton wrenchButton = myView.findViewById(R.id.wrenchButton);
+		wrenchButton.setOnClickListener(this);
+
+		//Room Side buttons
+		ToggleButton diningButton = myView.findViewById(R.id.diningButton);
+		diningButton.setOnClickListener(this);
+		ToggleButton hallButton = myView.findViewById(R.id.hallButton);
+		hallButton.setOnClickListener(this);
+		ToggleButton kitchenButton = myView.findViewById(R.id.kitchenButton);
+		kitchenButton.setOnClickListener(this);
+		ToggleButton ballroomButton = myView.findViewById(R.id.ballroomButton);
+		ballroomButton.setOnClickListener(this);
+		ToggleButton gameroomButton = myView.findViewById(R.id.gameroomButton);
+		gameroomButton.setOnClickListener(this);
+		ToggleButton gardenButton = myView.findViewById(R.id.gardenButton);
+		gardenButton.setOnClickListener(this);
+		ToggleButton libraryButton = myView.findViewById(R.id.libraryButton);
+		libraryButton.setOnClickListener(this);
+		ToggleButton loungeButton = myView.findViewById(R.id.loungeButton);
+		loungeButton.setOnClickListener(this);
+		ToggleButton studyButton = myView.findViewById(R.id.studyButton);
+		studyButton.setOnClickListener(this);
 
 		return myView;
 	}
@@ -39,9 +90,41 @@ public class accuseFragment extends Fragment implements View.OnClickListener
 	@Override
 	public void onClick(View button) {
 
+		float buttonInactive = (float) 0.3;
+		float buttonActive = 1;
+
 		switch (button.getId()) {
 			case R.id.cancelButtonAcc:
 				myActivity.getFragmentManager().beginTransaction().remove(this).commit();
+				
+				//Character Buttons
+			case R.id.scarletButton:
+			case R.id.peacockButton:
+			case R.id.greenButton:
+			case R.id.plumButton:
+			case R.id.whiteButton:
+			case R.id.mustardButton:
+				
+				//Weapon Side Buttons
+			case R.id.knifeButton:
+			case R.id.revolverButton:
+			case R.id.candlestickButton:
+			case R.id.leadpipeButton:
+			case R.id.ropeButton:
+			case R.id.wrenchButton:
+				
+				//Room Side Buttons
+			case R.id.diningButton:
+			case R.id.hallButton:
+			case R.id.kitchenButton:
+			case R.id.ballroomButton:
+			case R.id.gameroomButton:
+			case R.id.gardenButton:
+			case R.id.libraryButton:
+			case R.id.loungeButton:
+			case R.id.studyButton:
+				button.setAlpha(button.getAlpha() == buttonInactive ? buttonActive : buttonInactive);
+				break;
 		}
 
 		//Maybe use this to close:
