6df34d032d538c37b7f86eb12d54528fb4e1d1fc
Kai Richardson <kairichardson@gmail.com>
2020-04-12 23:40:22 -0700

    Adds working suggest view with dummy action. Needs to get the room the character is in still

--- app/src/main/java/edu/up/cs301/game/GameFramework/Clue/suggestFragment.java
+++ app/src/main/java/edu/up/cs301/game/GameFramework/Clue/suggestFragment.java
@@ -1,5 +1,6 @@
 package edu.up.cs301.game.GameFramework.Clue;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import edu.up.cs301.game.R;
@@ -17,6 +19,13 @@ public class suggestFragment extends Fragment implements View.OnClickListener
 
 	private Activity myActivity;
 
+	private Card chosenCharacter = null;
+	private Card chosenWeapon = null;
+	private Card chosenRoom = null;
+
+	float buttonInactive = (float) 0.5;
+	float buttonActive = 1;
+
 	@Override
 	public void onAttach(Activity act)
 	{
@@ -25,6 +34,8 @@ public class suggestFragment extends Fragment implements View.OnClickListener
 		this.myActivity = act;
 	}
 
+	//TODO: Pass/get the room the character is in and set the chosenRoom value
+	@SuppressLint("SetTextI18n")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
@@ -40,50 +51,47 @@ public class suggestFragment extends Fragment implements View.OnClickListener
 		//Character Side Buttons
 		ToggleButton scarletButton = myView.findViewById(R.id.scarletButton);
 		scarletButton.setOnClickListener(this);
+		scarletButton.setAlpha(buttonInactive);
 		ToggleButton peacockButton = myView.findViewById(R.id.peacockButton);
 		peacockButton.setOnClickListener(this);
+		peacockButton.setAlpha(buttonInactive);
 		ToggleButton greenButton = myView.findViewById(R.id.greenButton);
 		greenButton.setOnClickListener(this);
+		greenButton.setAlpha(buttonInactive);
 		ToggleButton plumButton = myView.findViewById(R.id.plumButton);
 		plumButton.setOnClickListener(this);
+		plumButton.setAlpha(buttonInactive);
 		ToggleButton whiteButton = myView.findViewById(R.id.whiteButton);
 		whiteButton.setOnClickListener(this);
+		whiteButton.setAlpha(buttonInactive);
 		ToggleButton mustardButton = myView.findViewById(R.id.mustardButton);
 		mustardButton.setOnClickListener(this);
+		mustardButton.setAlpha(buttonInactive);
 
 		//Weapon Side Buttons
 		ToggleButton knifeButton = myView.findViewById(R.id.knifeButton);
 		knifeButton.setOnClickListener(this);
+		knifeButton.setAlpha(buttonInactive);
 		ToggleButton revolverButton = myView.findViewById(R.id.revolverButton);
 		revolverButton.setOnClickListener(this);
+		revolverButton.setAlpha(buttonInactive);
 		ToggleButton candlestickButton = myView.findViewById(R.id.candlestickButton);
 		candlestickButton.setOnClickListener(this);
+		candlestickButton.setAlpha(buttonInactive);
 		ToggleButton ropeButton = myView.findViewById(R.id.ropeButton);
 		ropeButton.setOnClickListener(this);
+		ropeButton.setAlpha(buttonInactive);
 		ToggleButton leadpipeButton = myView.findViewById(R.id.leadpipeButton);
 		leadpipeButton.setOnClickListener(this);
+		leadpipeButton.setAlpha(buttonInactive);
 		ToggleButton wrenchButton = myView.findViewById(R.id.wrenchButton);
 		wrenchButton.setOnClickListener(this);
+		wrenchButton.setAlpha(buttonInactive);
 
-		//Room Side buttons
-		ToggleButton diningButton = myView.findViewById(R.id.diningButton);
-		diningButton.setOnClickListener(this);
-		ToggleButton hallButton = myView.findViewById(R.id.hallButton);
-		hallButton.setOnClickListener(this);
-		ToggleButton kitchenButton = myView.findViewById(R.id.kitchenButton);
-		kitchenButton.setOnClickListener(this);
-		ToggleButton ballroomButton = myView.findViewById(R.id.ballroomButton);
-		ballroomButton.setOnClickListener(this);
-		ToggleButton gameroomButton = myView.findViewById(R.id.gameroomButton);
-		gameroomButton.setOnClickListener(this);
-		ToggleButton gardenButton = myView.findViewById(R.id.gardenButton);
-		gardenButton.setOnClickListener(this);
-		ToggleButton libraryButton = myView.findViewById(R.id.libraryButton);
-		libraryButton.setOnClickListener(this);
-		ToggleButton loungeButton = myView.findViewById(R.id.loungeButton);
-		loungeButton.setOnClickListener(this);
-		ToggleButton studyButton = myView.findViewById(R.id.studyButton);
-		studyButton.setOnClickListener(this);
+		//Displays the current room
+		String location = chosenRoom.getName();
+		TextView locationDisplay = myView.findViewById(R.id.locationDisplay);
+		locationDisplay.setText(getString(R.string.locationDisplayPrefix) + location);
 
 		return myView;
 	}
@@ -95,43 +103,125 @@ public class suggestFragment extends Fragment implements View.OnClickListener
 		float buttonActive = 1;
 
 		switch (button.getId()) {
-			case R.id.cancelButtonSugg:
+			case R.id.cancelButtonAcc:
+				myActivity.getFragmentManager().beginTransaction().remove(this).commit();
+
+			case R.id.confirmButtonA:
+				//new ClueSuggestAction(getChosenCharacter(), getChosenWeapon(), getChosenRoom());
 				myActivity.getFragmentManager().beginTransaction().remove(this).commit();
 
 				//Character Buttons
 			case R.id.scarletButton:
+				if (chosenCharacter == null) {
+					chosenCharacter = new Card("Scarlet", 0);
+					button.setAlpha(buttonActive);
+				} else if (chosenCharacter.getName().equals("Scarlet")) {
+					chosenCharacter = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.peacockButton:
+				if (chosenCharacter == null) {
+					chosenCharacter = new Card("Peacock", 0);
+					button.setAlpha(buttonActive);
+				} else if (chosenCharacter.getName().equals("Peacock")) {
+					chosenCharacter = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.greenButton:
+				if (chosenCharacter == null) {
+					chosenCharacter = new Card("Green", 0);
+					button.setAlpha(buttonActive);
+				} else if (chosenCharacter.getName().equals("Green")) {
+					chosenCharacter = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.plumButton:
+				if (chosenCharacter == null) {
+					chosenCharacter = new Card("Plum", 0);
+					button.setAlpha(buttonActive);
+				} else if (chosenCharacter.getName().equals("Plum")) {
+					chosenCharacter = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.whiteButton:
+				if (chosenCharacter == null) {
+					chosenCharacter = new Card("White", 0);
+					button.setAlpha(buttonActive);
+				} else if (chosenCharacter.getName().equals("White")) {
+					chosenCharacter = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.mustardButton:
+				if (chosenCharacter == null) {
+					chosenCharacter = new Card("Mustard", 0);
+					button.setAlpha(buttonActive);
+				} else if (chosenCharacter.getName().equals("Mustard")) {
+					chosenCharacter = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 
-				//Weapon Side Buttons
+			//Weapon Side Buttons
 			case R.id.knifeButton:
+				if (chosenWeapon == null) {
+					chosenWeapon = new Card("Knife", 1);
+					button.setAlpha(buttonActive);
+				} else if (chosenWeapon.getName().equals("Knife")) {
+					chosenWeapon = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.revolverButton:
+				if (chosenWeapon == null) {
+					chosenWeapon = new Card("Revolver", 1);
+					button.setAlpha(buttonActive);
+				} else if (chosenWeapon.getName().equals("Revolver")) {
+					chosenWeapon = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.candlestickButton:
+				if (chosenWeapon == null) {
+					chosenWeapon = new Card("Candlestick", 1);
+					button.setAlpha(buttonActive);
+				} else if (chosenWeapon.getName().equals("Candlestick")) {
+					chosenWeapon = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.leadpipeButton:
+				if (chosenWeapon == null) {
+					chosenWeapon = new Card("LeadPipe", 1);
+					button.setAlpha(buttonActive);
+				} else if (chosenWeapon.getName().equals("LeadPipe")) {
+					chosenWeapon = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.ropeButton:
+				if (chosenWeapon == null) {
+					chosenWeapon = new Card("Rope", 1);
+					button.setAlpha(buttonActive);
+				} else if (chosenWeapon.getName().equals("Rope")) {
+					chosenWeapon = null;
+					button.setAlpha(buttonInactive);
+				}
+				break;
 			case R.id.wrenchButton:
-
-				//Room Side Buttons
-			case R.id.diningButton:
-			case R.id.hallButton:
-			case R.id.kitchenButton:
-			case R.id.ballroomButton:
-			case R.id.gameroomButton:
-			case R.id.gardenButton:
-			case R.id.libraryButton:
-			case R.id.loungeButton:
-			case R.id.studyButton:
-				button.setAlpha(button.getAlpha() == buttonInactive ? buttonActive : buttonInactive);
+				if (chosenWeapon == null) {
+					chosenWeapon = new Card("Wrench", 1);
+					button.setAlpha(buttonActive);
+				} else if (chosenWeapon.getName().equals("Wrench")) {
+					chosenWeapon = null;
+					button.setAlpha(buttonInactive);
+				}
 				break;
 		}
-
-		//Maybe use this to close:
-		// if confirm button
-		// call method on main activity then:
-		// getActivity().getFragmentManager().beginTransaction().remove(this).commit();
 
 	}
 }
